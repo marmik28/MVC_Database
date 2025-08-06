e6e2601fbee0e2908967b3555cf9f11405538aff
marmik28112002 <46027029-marmik28112002@users.noreply.replit.com>
2025-08-06 20:07:35 +0000

    Restored to '2c2c0b5bdab6cd21ede3e09d8551cc4dd8079ba2'
    Replit-Restored-To: 2c2c0b5bdab6cd21ede3e09d8551cc4dd8079ba2

--- server/db.ts
+++ server/db.ts
@@ -3,7 +3,6 @@ import { drizzle } from 'drizzle-orm/neon-serverless';
 import ws from "ws";
 import * as schema from "@shared/schema";
 
-
 neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
